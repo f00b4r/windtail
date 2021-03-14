a43c16c268c34897e17ea2541d1eedeb84e9a073
Milan Felix Šulc <rkfelix@gmail.com>
2021-03-14 13:41:27 +0100

    Arcgis: added primary and secondary color

--- src/arcgis/tailwind.config.js
+++ src/arcgis/tailwind.config.js
@@ -3,4 +3,12 @@ const base = require('../tailwind.config');
 
 module.exports = resolve(base, {
   prefix: 'tw-',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#c4001e',
+        secondary: '#2b2b82',
+      }
+    }
+  }
 });
